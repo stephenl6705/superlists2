6850a9f71f47f3a3fe1b22df917e4ee3da24a91b
stephenl6705 <stephen.langestraat@gmail.com>
2016-04-15 05:42:29 +0000

    adding in javascript for logout - not working, so didn't activate

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -4,18 +4,28 @@ var initialize = function(navigator, user, token, urls) {
     $('#id_login').on('click', function() {
         navigator.id.request();
     });
+    $('#id_logout').on('click', function() {
+        navigator.id.logout();
+    });
 
     navigator.id.watch({
         loggedInUser: user,
         onlogin: function (assertion) {
-            var deferred = $.post(
+            $.post(
                 urls.login,
                 { assertion: assertion, csrfmiddlewaretoken: token }
-            );
-            deferred.done(function () { window.location.reload(); })
-            deferred.fail(function () { navigator.id.logout(); });
+            )
+                .done(function () { window.location.reload(); })
+                .fail(function () { navigator.id.logout(); });
         },
-        onlogout: function () {}
+        onlogout: function () {
+            $.post(
+                urls.logout,
+                { csrfmiddlewaretoken: token }
+            )
+                .done(function () { window.location.reload(); })
+                .fail(function () { navigator.id.logout(); });
+        }
     });
 };
 window.Superlists = {
