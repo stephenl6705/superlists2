108be3023202305cac7124c865062f9015a506f5
stephenl6705 <stephen.langestraat@gmail.com>
2016-04-15 00:11:11 +0000

    add screenshot on failure to FT runner

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -17,8 +17,21 @@ class LoginTest(FunctionalTest):
 
     def wait_for_element_with_id(self, element_id):
         WebDriverWait(self.browser, timeout=30).until(
-            lambda b: b.find_element_by_id(element_id)
+            lambda b: b.find_element_by_id(element_id),
+            'Could not find element with id {}. Page text was:\n{}'.format(
+                element_id, self.browser.find_element_by_tag_name('body').text
+            )
         )
+
+    def wait_to_be_logged_in(self):
+        self.wait_for_element_with_id('id_logout')
+        navbar = self.browser.find_element_with_css_selector('.navbar')
+        self.assertIn('edith@mockmyid.com', navbar.text)
+
+    def wait_to_be_logged_out(self):
+        self.wait_for_element_with_id('id_login')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn('edith@mockmyid.com', navbar.text)
 
     def test_login_with_persona(self):
         # Edith goes to the awesome superlists site
@@ -43,3 +56,28 @@ class LoginTest(FunctionalTest):
         self.wait_for_element_with_id('id_logout')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn('edith@mockmyid.com', navbar.text)
+
+        # not just a one-off for that page
+        self.browser.refresh()
+        self.wait_for_element_with_id('id_logout')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn('edith@mockmyid.com', navbar.text)
+
+        # The persona window closes
+        self.switch_to_new_window('To-Do')
+
+        # She can see that she is logged in
+        self.wait_to_be_logged_in()
+
+        # Refreshing the page, she sees it's a real session login,
+        # not just aone-off for that page
+        self.browser.refresh()
+        self.wait_to_be_logged_in()
+
+        # Terrified of this new feature, she reflexively clicks "logout"
+        self.browser.find_element_by_id('id_logout').click()
+        self.wait_to_be_logged_out()
+
+        # The "logged out" status also persists after a refresh
+        self.browser.refresh()
+        self.wait_to_be_logged_out()
