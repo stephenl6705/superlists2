dffaeb9b82324af01e0e1cd0016298d7e4d575e6
stephenl6705 <stephen.langestraat@gmail.com>
2016-04-15 09:39:14 +0000

    placeholder test_my_lists and move ligin checkers into base + remove javascript for logout

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -4,9 +4,6 @@ var initialize = function(navigator, user, token, urls) {
     $('#id_login').on('click', function() {
         navigator.id.request();
     });
-    $('#id_logout').on('click', function() {
-        navigator.id.logout();
-    });
 
     navigator.id.watch({
         loggedInUser: user,
@@ -18,14 +15,6 @@ var initialize = function(navigator, user, token, urls) {
                 .done(function () { window.location.reload(); })
                 .fail(function () { navigator.id.logout(); });
         },
-        onlogout: function () {
-            $.post(
-                urls.logout,
-                { csrfmiddlewaretoken: token }
-            )
-                .done(function () { window.location.reload(); })
-                .fail(function () { navigator.id.logout(); });
-        }
     });
 };
 window.Superlists = {
