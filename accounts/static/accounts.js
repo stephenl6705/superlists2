3429521f07bf95d7e558651f8b1867d44faa0cdb
stephenl6705 <stephen.langestraat@gmail.com>
2016-04-22 04:29:28 +0000

    Fiz auto re-login bug.

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -4,17 +4,27 @@ var initialize = function(navigator, user, token, urls) {
         onlogin: function (assertion) {
             $.post(
                 urls.login,
-                { assertion: assertion, csrfmiddlewaretoken: token}
+                { assertion: assertion, csrfmiddlewaretoken: token }
             )
                 .done(function () {window.location.reload(); })
                 .fail(function () { navigator.id.logout(); });
         },
-        onlogout: function () {}
+        onlogout: function () {
+            $.post(
+                urls.logout,
+                { csrfmiddlewaretoken: token }
+            )
+                .done(function () {window.location.reload(); })
+                .fail(function () { navigator.id.logout(); });
+        }
     });
     $('#id_login').on('click', function() {
         navigator.id.request();
     });
-    
+    $('#id_logout').on('click', function() {
+        navigator.id.logout();
+    });
+
 };
 window.Superlists = {
     Accounts: {
