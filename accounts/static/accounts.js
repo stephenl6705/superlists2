57810b596ea8bee2b8da49f8e44fe6fc591bfb27
stephenl6705 <stephen.langestraat@gmail.com>
2016-04-22 00:53:44 +0000

    fixed error in account.js

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -1,22 +1,20 @@
-/*global $ */
-
 var initialize = function(navigator, user, token, urls) {
     navigator.id.watch({
         loggedInUser: user,
         onlogin: function (assertion) {
             $.post(
                 urls.login,
-                { assertion: assertion, csrfmiddlewaretoken: token }
+                { assertion: assertion, csrfmiddlewaretoken: token}
             )
-                .done(function () { window.location.reload(); })
+                .done(function () {window.location.reload(); })
                 .fail(function () { navigator.id.logout(); });
         },
+        onlogout: function () {}
     });
-
     $('#id_login').on('click', function() {
         navigator.id.request();
     });
-
+    
 };
 window.Superlists = {
     Accounts: {
